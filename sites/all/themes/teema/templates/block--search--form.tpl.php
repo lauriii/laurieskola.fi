7748f8c3a9ae45cb8f9af47bc977da4ea5db8755
Lauri Eskola <lauri.eskola@gmail.com>
2014-01-26 16:41:19 +0200

    Initial commit

---
+++ sites/all/themes/teema/templates/block--search--form.tpl.php
@@ -0,0 +1,3 @@
+<div id="block-<?php print $block->module .'-'. $block->delta ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <?php print $content; ?>
+</div> <!-- /block-inner /block -->
